d3d2006c55ced43cf69f37e5ec61a888a46c5a23
Eriife-git <eriifefashonista22@gmail.com>
2025-08-17 13:58:28 +0100

    response

---
+++ src/main/java/com/pharmacy/dtos/responses/AddDrugResponse.java
@@ -0,0 +1,23 @@
+package com.pharmacy.dtos.responses;
+
+public class AddDrugResponse {
+    private int drugId;
+    private String drugName;
+
+    public int getDrugId() {
+        return drugId;
+    }
+
+    public void setDrugId(int drugId) {
+        this.drugId = drugId;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+}
+
