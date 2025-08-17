f401179c65b7b280fa708b566788e3f79b06ce24
justman0898 <jayigboneme@gmail.com>
2025-08-17 04:51:50 -0700

    Added Drug Model

---
+++ src/main/java/com/pharmacy/data/models/DrugType.java
@@ -0,0 +1,21 @@
+package com.pharmacy.data.models;
+
+public enum DrugType {
+
+    SYRUP(50), CAPSULE(11), INJECTION(20),
+    IV(10), TABLET(11);
+
+    private int amount;
+
+    DrugType(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
