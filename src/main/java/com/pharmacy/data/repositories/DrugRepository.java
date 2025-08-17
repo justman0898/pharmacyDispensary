6be474c2727ad32b1975727da1a56d909fa43a61
justman0898 <jayigboneme@gmail.com>
2025-08-17 09:31:35 -0700

    Drug Repository Completed

--- src/main/java/com/pharmacy/data/repositories/DrugRepository.java
+++ src/main/java/com/pharmacy/data/repositories/DrugRepository.java
@@ -2,14 +2,16 @@ package com.pharmacy.data.repositories;
 
 import com.pharmacy.data.models.Drug;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DrugRepository {
 
     Drug save (Drug drug);
     void deleteById(int id);
-    void deleteAll(int id);
+    void deleteAll();
     Optional<Drug>  findById(int id);
+    List<Drug> findAll();
 
 
 }
--- src/test/java/com/pharmacy/resources/drugs.sql
+++ src/test/java/com/pharmacy/resources/drugs.sql
@@ -3,7 +3,8 @@ CREATE TABLE drugs (
                        drug_name VARCHAR(50),
                        drug_category VARCHAR(50),
                        drug_type VARCHAR(50),
-                       expiry_date TIMESTAMP,
-                       date_created TIMESTAMP,
+                       expiry_date DATE,
+                       date_created DATE,
+                       manufacture_date DATE,
                        quantity BIGINT
                    );
