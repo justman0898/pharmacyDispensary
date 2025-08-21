05860ed6e04272991edd9f57e5f4d85b67a16cf5
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2025-08-21 12:55:43 +0100

    Commit UserRepository

---
+++ src/main/java/com/pharmacy/data/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.pharmacy.data.repositories;
+
+import com.pharmacy.data.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    User save(User user);
+    void delete(User user);
+    Optional<User> findByUsername(String username);
+    List<User> findAll();
+
+}
