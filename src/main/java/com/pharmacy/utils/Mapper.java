101e530b55589273bf5d1a816d75e072245052c0
Eriife-git <eriifefashonista22@gmail.com>
2025-08-21 07:23:13 +0100

    Mapper

---
+++ src/main/java/com/pharmacy/utils/Mapper.java
@@ -0,0 +1,28 @@
+package com.pharmacy.utils;
+
+import com.pharmacy.data.models.Drug;
+import com.pharmacy.dtos.requests.AddDrugRequest;
+import com.pharmacy.dtos.responses.AddDrugResponse;
+
+public class Mapper {
+
+    public static Drug convertToDrug(AddDrugRequest addDrugRequest) {
+        Drug drug = new Drug();
+        drug.setDrugName(addDrugRequest.getDrugName());
+        drug.setDrugtype(addDrugRequest.getDrugtype());
+        drug.setDrugCategory(addDrugRequest.getDrugCategory());
+        drug.setQuantity(addDrugRequest.getQuantity());
+        drug.setDateCreated(addDrugRequest.getDateCreated());
+        drug.setExpiryDate(addDrugRequest.getExpiryDate());
+        return drug;
+    }
+
+
+    public static AddDrugResponse convertToResponse(Drug savedDrug){
+        AddDrugResponse addDrugResponse =  new AddDrugResponse();
+        addDrugResponse.setDrugId(savedDrug.getDrugId());
+        addDrugResponse.setDrugName(savedDrug.getDrugName());
+        return addDrugResponse;
+    }
+
+}
