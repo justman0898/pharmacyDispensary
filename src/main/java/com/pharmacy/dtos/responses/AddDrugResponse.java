0df357e9821f943c303621c3429b19c097bd08b8
Eriife-git <eriifefashonista22@gmail.com>
2025-08-21 22:43:14 +0100

    Updated changes

--- src/main/java/com/pharmacy/dtos/responses/AddDrugResponse.java
+++ src/main/java/com/pharmacy/dtos/responses/AddDrugResponse.java
@@ -1,8 +1,20 @@
 package com.pharmacy.dtos.responses;
 
+import com.pharmacy.data.models.DrugCategory;
+import com.pharmacy.data.models.DrugType;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class AddDrugResponse {
     private int drugId;
     private String drugName;
+    private DrugCategory drugCategory;
+    private DrugType drugType;
+    private java.sql.Date expiryDate;
+    private java.sql.Date dateCreated = Date.valueOf(LocalDate.now());
+    private java.sql.Date manufactureDate;
+    private long quantity;
 
 
     public int getDrugId() { return drugId;}
@@ -19,6 +31,47 @@ public class AddDrugResponse {
         this.drugName = drugName;
     }
 
+    public DrugType getDrugType() {return drugType; }
+
+    public void setDrugType(DrugType drugType) { this.drugType = drugType; }
+
+    public Date getExpiryDate() { return expiryDate; }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public DrugCategory getDrugCategory() {
+        return drugCategory;
+    }
+
+    public void setDrugCategory(DrugCategory drugCategory) {
+        this.drugCategory = drugCategory;
+    }
 
 }
 
