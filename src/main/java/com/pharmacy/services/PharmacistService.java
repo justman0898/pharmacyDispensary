f7b7f2686a87cd8d8f6e51403be61a695cea211e
Eriife-git <eriifefashonista22@gmail.com>
2025-08-22 14:07:33 +0100

    Updated changes

--- src/main/java/com/pharmacy/services/PharmacistService.java
+++ src/main/java/com/pharmacy/services/PharmacistService.java
@@ -1,6 +1,7 @@
 package com.pharmacy.services;
 
 import com.pharmacy.data.models.Prescription;
+import com.pharmacy.dtos.responses.AddPrescriptionResponse;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ public interface PharmacistService {
     boolean login(int pharmacistId, String password);
     Prescription verifyPrescription(int prescriptionId);
     Prescription dispensePrescription(int prescriptionId);
-    List<Prescription> viewDispensedHistory(int pharmacistId);
+    List<AddPrescriptionResponse> viewDispensedHistory();
+    List<AddPrescriptionResponse> viewUnresolvedHistory();
 }
