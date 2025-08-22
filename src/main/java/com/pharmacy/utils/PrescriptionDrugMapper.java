59caaa921d153efe5b0db92592877cc25a476d00
justman0898 <jayigboneme@gmail.com>
2025-08-22 05:44:48 -0700

    prescriptionResponse

--- src/main/java/com/pharmacy/utils/PrescriptionDrugMapper.java
+++ src/main/java/com/pharmacy/utils/PrescriptionDrugMapper.java
@@ -1,8 +1,10 @@
 package com.pharmacy.utils;
 
 import com.pharmacy.data.models.Drug;
+import com.pharmacy.data.models.Prescription;
 import com.pharmacy.dtos.requests.AddPrescriptionDrugRequest;
 import com.pharmacy.dtos.requests.AddPrescriptionRequest;
+import com.pharmacy.dtos.responses.AddPrescriptionResponse;
 
 public class PrescriptionDrugMapper {
 
@@ -11,7 +13,21 @@ public class PrescriptionDrugMapper {
         drug.setDrugId(req.getDrugId());
         drug.setQuantity(req.getQuantityPrescribed());
         return drug;
-
-
     }
+
+    public static AddPrescriptionResponse toResponse(Prescription prescription) {
+        AddPrescriptionResponse response = new AddPrescriptionResponse();
+        response.setPrescriptionId(prescription.getPrescriptionId());
+        response.setPatientId(prescription.getPatientId());
+        response.setPatientName(prescription.getPatientName());
+        response.setDoctorId(prescription.getDoctorId());
+        response.setDoctorName(prescription.getDoctorName());
+        response.setDiagnosis(prescription.getDiagnosis());
+        response.setDateCreated(prescription.getDateCreated());
+        response.setResolved(prescription.isResolved());
+        response.setDrugsPrescribed(prescription.getDrugsPrescribed());
+        return response;
+    }
+
+
 }
